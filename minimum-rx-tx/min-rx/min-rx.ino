e21687ce2bf0508671b632b10ba536fddbd4ceeb
Michael Cheich <mcheich@gmail.com>
2022-08-19 09:49:32 -0400

    min tx rx code testing

--- minimum-rx-tx/min-rx/min-rx.ino
+++ minimum-rx-tx/min-rx/min-rx.ino
@@ -1,3 +1,6 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
 void setup() {}
 
 void loop() {}
--- minimum-rx-tx/min-tx/min-tx.ino
+++ minimum-rx-tx/min-tx/min-tx.ino
@@ -1,3 +1,118 @@
-void setup() {}
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Global copy of receiver
+esp_now_peer_info_t receiver;
+
+#define CHANNEL 1
+#define PRINTSCANRESULTS 0
+#define DELETEBEFOREPAIR 0
+
+/* Init ESP Now with fallback */
+void InitESPNow()
+{
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK)
+  {
+    Serial.println("ESPNow Init Success");
+  }
+  else
+  {
+    Serial.println("ESPNow Init Failed");
+    ESP.restart();
+  }
+}
+
+// Scan for receivers in AP mode
+void ScanForRXs()
+{
+  int8_t scanResults = WiFi.scanNetworks();
+  // reset on each scan
+  bool receiverFound = 0;
+  memset(&receiver, 0, sizeof(receiver));
+
+  Serial.println("");
+  if (scanResults == 0)
+  {
+    Serial.println("No WiFi devices in AP Mode found");
+  }
+  else
+  {
+    Serial.print("Found ");
+    Serial.print(scanResults);
+    Serial.println(" devices ");
+    for (int i = 0; i < scanResults; ++i)
+    {
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+      if (PRINTSCANRESULTS)
+      {
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(SSID);
+        Serial.print(" (");
+        Serial.print(RSSI);
+        Serial.print(")");
+        Serial.println("");
+      }
+      delay(10);
+      // Check if the current device starts with `Slave`
+      if (SSID.indexOf("Slave") == 0)
+      {
+        // SSID of interest
+        Serial.println("Found a Slave.");
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(SSID);
+        Serial.print(" [");
+        Serial.print(BSSIDstr);
+        Serial.print("]");
+        Serial.print(" (");
+        Serial.print(RSSI);
+        Serial.print(")");
+        Serial.println("");
+        // Get BSSID => Mac Address of the Slave
+        int mac[6];
+        if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]))
+        {
+          for (int ii = 0; ii < 6; ++ii)
+          {
+            receiver.peer_addr[ii] = (uint8_t)mac[ii];
+          }
+        }
+
+        receiver.channel = CHANNEL; // pick a channel
+        receiver.encrypt = 0;       // no encryption
+
+        receiverFound = 1;
+        // we are planning to have only one slave in this example;
+        // Hence, break after we find one, to be a bit efficient
+        break;
+      }
+    }
+  }
+
+  if (receiverFound)
+  {
+    Serial.println("Slave Found, processing..");
+  }
+  else
+  {
+    Serial.println("Slave Not Found, trying again.");
+  }
+
+  // clean up ram
+  WiFi.scanDelete();
+}
+
+void setup() {
+
+  InitESPNow();
+  ScanForRXs();
+  
+}
 
 void loop() {}
